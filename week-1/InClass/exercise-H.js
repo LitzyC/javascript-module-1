2a9a7ca80ea93f7f39bb58c1e0fa51ebc393cf50
litzycabrera <litzy.prinss1@gmail.com>
2022-08-18 17:52:26 +0200

    Ejercicio H, hecho.

--- week-1/InClass/exercise-H.js
+++ week-1/InClass/exercise-H.js
@@ -1,5 +1,43 @@
-let num1 = prompt.length('Ingresa el primer numero a calcular.');
-let num2 = prompt.length('Ingresa el segundo numero a calcular.');
+
+function suma(num1, num2){
+  const resultado = parseInt(num1) + parseInt(num2);
+  console.log("La suma es de: "+resultado);
+  return resultado;
+}
+
+suma(11, 25);
+
+
+
+
+
+
+/*const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`Ingresa el primer numero a calcular.`, num1, (`Ingresa el segundo numero a calcular.`, num2=> {
+    console.log("La suma es de:" +num1 + +num2);
+    readline.close();
+}));
+
+let num1 = setprom('Ingresa el primer numero a calcular.');
+let num2 = Readline('Ingresa el segundo numero a calcular.');
+
+function suma(num1, num2){
+  const resultado = Number(num1) + Number(num2);
+  console.log("La suma es de: "+resultado);
+  return resultado;
+}
+
+suma(num1, num2);
+
+const prompt = require("prompt");
+const { rawListeners } = require('process');
+
+let num1 = prompt('Ingresa el primer numero a calcular.');
+let num2 = prompt('Ingresa el segundo numero a calcular.');
 
 function suma(num1, num2){
   const resultado = parseInt(num1) + parseInt(num2);
@@ -8,3 +46,30 @@ function suma(num1, num2){
 }
 
 suma(num1, num2);
+
+
+
+const readline = require('readline').createInterface({
+input:process.stdin, 
+output: process.stdin
+});
+
+let num1=Math.floor((Math.random()+1)+1);
+let num2=Math.floor((Math.random()+1)+1);
+let answer=num1+ num2;
+
+rl.question(`Cual es la suma ${num1} + ${num2}? `,
+(userInput)=>{
+  if(userInput.trim()== answer){
+    rl.close();
+  }
+});
+
+rl.on('close',()=>{
+    console.log('Correcto');
+}else{
+  rl.setprompt('Incorrecto');
+  rl.prompt();
+});
+
+*/
