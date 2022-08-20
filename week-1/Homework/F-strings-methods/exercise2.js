9dd02ba5f3e0e0c9330047ffce4d3c6458ad4a80
litzycabrera <litzy.prinss1@gmail.com>
2022-08-20 12:46:41 +0200

    Ejercicio 2, hecho.

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,5 @@
-const name = " Daniel  ";
+const nombre = " Daniel  ";
+const espacio=nombre.trim();
+const contada=espacio.length;
 
-console.log(message);
+console.log("My name is" +nombre+ "and my name is "+ contada + " characteres long.");
