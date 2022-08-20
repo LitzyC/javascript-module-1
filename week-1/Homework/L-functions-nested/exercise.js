3f7a90468546077cd4e0522f868375cb91f1bc0a
litzycabrera <litzy.prinss1@gmail.com>
2022-08-20 13:56:11 +0200

    Ejercicio 1, hecho.

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,13 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+const arrayMentors = [mentor1, mentor2, mentor3, mentor4, mentor5];
+
+function greeting(arrayMentors){
+    arrayMentors.forEach(mentor => {
+        console.log("HELLO "+ mentor.toUpperCase())
+    });
+}
+
+greeting(arrayMentors);
