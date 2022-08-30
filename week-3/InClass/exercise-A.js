842059378e9e93ec5f3a77b015b63c4de03ecfe4
litzycabrera <litzy.prinss1@gmail.com>
2022-08-30 19:04:16 +0200

    Ejercicio A, hecho.

---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,7 @@
+const members = ['Andrea', 'Karelys', 'Linda', 'Graciela', 'Luis', 'Gabriel'];
+
+members.unshift('Xiomara');
+members.push('Natalia');
+
+console.log(members);
+console.log(members.length);
